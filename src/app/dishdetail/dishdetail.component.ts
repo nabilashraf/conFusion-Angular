edd652d98242c25b5f7917dac43b3212606bf666
Nabil Ashraf <nabeelashraf.2008@gmail.com>
2017-12-09 23:20:25 +0500

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,8 +6,7 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-
+import { visibility, expand } from '../animations/app.animation';
 import { Comment } from '../shared/comment';
 
 import 'rxjs/add/operator/switchMap';
@@ -17,17 +16,8 @@ import 'rxjs/add/operator/switchMap';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    expand()
   ]
 })
 
@@ -46,7 +36,7 @@ export class DishdetailComponent implements OnInit {
   };
   errMess: string;
   visibility = 'shown';
-  
+
   validationMessages = {
     'author': {
       'required':      'Author Name is required.',
